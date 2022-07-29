c0d175ca1caea9003229cbe0e76eac61b3380382
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-07-29 13:03:13 +0000

    Create tryOut.js

---
+++ tryOut.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const tryOut = (
+ 
+     <div>
+     <h1> Fun facts about react</h1>
+     
+    <ul> 
+    <li> React is a javascript library created in 2013 </li>
+    <li> It is maintained by Facebook </li>
+    <li> React is fun to write </li>
+    <li> React was originally created by John Walke </li>
+    
+    </div>
+)
+
+ReactDOM.render(tryOut, document.getElementById("root"))
